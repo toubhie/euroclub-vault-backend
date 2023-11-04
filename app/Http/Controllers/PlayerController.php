26f0dc83a691d45ac7f196c0e0fefedf2b0a43f8
toubhie <tobiwily@yahoo.com>
2023-11-04 15:01:35 +0000

    initial commit

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    public function index(){
+        return Player::all();
+    }
+
+    public function store(Request $request){
+        return Player::create($request->all());
+    }
+
+    public function show($id){
+        return Player::find($id);
+    }
+
+    public function update(Request $request, $id){
+        $player = Player::find($id);
+        $player->update($request->all());
+        return $player;
+    }
+
+    public function destroy($id){
+        $player = Player::find($id);
+        $player->delete();
+        return ['message' => 'Player deleted'];
+    }
+}
